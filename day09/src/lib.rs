5e8fd553a234758d45d419b20594b323a30b18a5
PurpleMyst <PurpleMyst@users.noreply.github.com>
2022-12-11 18:59:28 +0100

    perf(day09): optimize with better touching calcolation + fnv hasher

--- day09/src/lib.rs
+++ day09/src/lib.rs
@@ -1,29 +1,53 @@
-use std::io::stdout;
-use std::{collections::HashSet, fmt::Display};
-
-use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
-use crossterm::{
-    cursor,
-    style::{Attribute, Color, Stylize},
-    ExecutableCommand,
-};
+use std::fmt::Display;
 
 type Point = (i64, i64);
 
-fn touching(head: Point, tail: Point) -> bool {
-    (-1..=1)
-        .flat_map(|dx| (-1..=1).map(move |dy| (head.0 + dx, head.1 + dy)))
-        .any(|p| p == tail)
+const PART2_KNOTS: usize = 10;
+
+fn touching((hx, hy): Point, (tx, ty): Point) -> bool {
+    hx.abs_diff(tx) <= 1 && hy.abs_diff(ty) <= 1
 }
 
 fn next_tail_pos((hx, hy): Point, (tx, ty): Point) -> Point {
     (tx + (hx - tx).signum(), ty + (hy - ty).signum())
 }
 
-fn simulate<const KNOT_AMOUNT: usize>() -> usize {
-    let mut knots = [(0, 0); KNOT_AMOUNT];
+fn simulate_part1() -> usize {
+    let mut head = (0, 0);
+    let mut tail = (0, 0);
 
-    let mut visited = HashSet::new();
+    let mut visited = fnv::FnvHashSet::default();
+    visited.insert(tail);
+
+    // stdout().execute(EnterAlternateScreen).unwrap();
+    include_str!("input.txt")
+        .lines()
+        .map(|line| line.split_once(' ').unwrap())
+        .for_each(|(dir, amount)| {
+            let (dx, dy) = match dir {
+                "U" => (0, 1),
+                "D" => (0, -1),
+                "L" => (-1, 0),
+                "R" => (1, 0),
+                _ => unreachable!(),
+            };
+            for _ in 0..amount.parse::<u8>().unwrap() {
+                let new_head = (head.0 + dx, head.1 + dy);
+                if !touching(tail, new_head) {
+                    tail = head;
+                }
+                head = new_head;
+                visited.insert(tail);
+            }
+        });
+
+    visited.len()
+}
+
+fn simulate_part2() -> usize {
+    let mut knots = [(0, 0); PART2_KNOTS];
+
+    let mut visited = fnv::FnvHashSet::default();
     visited.insert(knots.last().copied().unwrap());
 
     // stdout().execute(EnterAlternateScreen).unwrap();
@@ -41,7 +65,7 @@ fn simulate<const KNOT_AMOUNT: usize>() -> usize {
             for _ in 0..amount.parse::<u8>().unwrap() {
                 let (hx, hy) = knots.first().copied().unwrap();
                 knots[0] = (hx + dx, hy + dy);
-                for i in 1..KNOT_AMOUNT {
+                for i in 1..PART2_KNOTS {
                     if !touching(knots[i - 1], knots[i]) {
                         knots[i] = next_tail_pos(knots[i - 1], knots[i]);
                     }
@@ -55,5 +79,5 @@ fn simulate<const KNOT_AMOUNT: usize>() -> usize {
 
 #[inline]
 pub fn solve() -> (impl Display, impl Display) {
-    (simulate::<2>(), simulate::<10>())
+    (simulate_part1(), simulate_part2())
 }
